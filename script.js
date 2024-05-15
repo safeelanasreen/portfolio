fdc0da326df41c3cbbb531c48b52c68f304de417
safeela nasreen <safeela.nasreen@webandcrafts.com>
2024-05-15 10:38:22 +0530

    counter

--- script.js
+++ script.js
@@ -96,4 +96,27 @@ window.addEventListener("load", function () {
 });
 
 
- 
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const counter = entry.target;
+        let endValue = counter.textContent;
+        let startValue = 0;
+        let updating = setInterval(() => {
+          startValue += endValue / 200;
+          counter.textContent = startValue.toFixed(0);
+          if (startValue > endValue) {
+            counter.textContent = endValue;
+            clearInterval(updating);
+            observer.unobserve(counter);
+          }
+        }, 10);
+      }
+    });
+  },
+  { threshold: 1 }
+);
+document
+  .querySelectorAll(".counter")
+  .forEach((counter) => observer.observe(counter));
